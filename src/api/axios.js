a077c4d8e6dd922afcd5afdc2a3edf55f14680f0
Felipe Dulcey <fdulcey@unicauca.edu.co>
2024-01-24 15:41:31 -0500

    cambio url 1.3

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+// baseURL: 'http://localhost:3000/api',
+// baseURL: 'https://valkiria-backend-felipe-dulceys-projects.vercel.app/api',
+
 const instance = axios.create({
-    // baseURL: 'http://localhost:3000/api',
-    // baseURL: 'https://valkiria-backend-felipe-dulceys-projects.vercel.app/api',
     baseURL: 'https://valkiria-backend-felipe-dulceys-projects.vercel.app/api',
     withCredentials: true,
 })
