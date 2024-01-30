51b7eb8d7a6c20b0904182d2bcfb0224f760d490
Felipe Dulcey <fdulcey@unicauca.edu.co>
2024-01-30 17:15:00 -0500

    Facebook 4.2

--- src/componentes/Facebook/Button.jsx
+++ src/componentes/Facebook/Button.jsx
@@ -31,7 +31,7 @@ function Button(onLogin) {
           };
   
           // Almacena el usuario en local storage
-          window.localStorage.setItem("user", JSON.stringify(user));
+          window.localStorage.setItem("user", JSON.stringify(user.accessToken));
   
           // Llama a la función onLogin con la información del usuario
           onLogin(user);
