e7fda2801b356bcb8811090a7b9c5e1af19a97fc
Felipe Dulcey <fdulcey@unicauca.edu.co>
2024-01-30 15:05:41 -0500

    Facebook 2.2

--- src/pages/LoginRegister.jsx
+++ src/pages/LoginRegister.jsx
@@ -10,7 +10,7 @@ import { Button } from "../componentes/Facebook/Button";
 // import * as storage from "../componentes/Facebook/storage";
 
 function LoginRegister() {
-  
+
   const onLogin = (user) => {
     // Almacenar en el localStorage
     console.log(user);
