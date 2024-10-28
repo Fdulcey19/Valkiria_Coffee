a15b98c6e646e268bc71d655e1e511f2c4b26656
Felipe Dulcey <fdulcey@unicauca.edu.co>
2024-10-28 17:49:11 -0500

    cambios al servidore de render y rutas del nuevo servidor

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // baseURL: 'https://valkiria-backend-felipe-dulceys-projects.vercel.app/api',
 const instance = axios.create({
-    baseURL: 'https://valkiria-backend.vercel.app/api/',
+    baseURL: 'https://valkiria.onrender.com/api/',
 })
 
 export default instance;
--- src/domain/Price.js
+++ src/domain/Price.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getPrecios = async () => {
     try {
         // const responseAllData = await axios.get("https://valkiria-backend-felipe-dulceys-projects.vercel.app/api/precios");
-        const responseAllData = await axios.get("https://valkiria-backend.vercel.app/api/precios");
+        const responseAllData = await axios.get("https://valkiria.onrender.com/api/precios");
         return responseAllData;
     } catch {
         console.log("Error al obtener los datos".error);
